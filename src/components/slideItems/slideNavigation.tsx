e796cf97d651aa90972daf810d5d6372772a543d
gakin95 <gakin95@gmail.com>
2021-09-27 21:49:17 +0100

    created SlideNavigation component

--- src/components/slideItems/slideNavigation.tsx
+++ src/components/slideItems/slideNavigation.tsx
@@ -2,44 +2,78 @@ import Drawer from "@material-ui/core/Drawer";
 import { makeStyles } from "@material-ui/core/styles";
 import GridLayout, { Layout } from "react-grid-layout";
 
-import { PhotoImage } from './photo';
+import { PhotoImage } from "./photo";
 import { Video } from "./video";
 import { TextContainer } from "./text";
-import { ActionProp } from '../model';
-
+import { ActionProp } from "../model";
 
 export type SlideNavigatorProp = {
-    backgroundColor: string;
-    layout:any,
-    gridItems:any,
-    onLayoutChange?(layout: GridLayout.Layout[]): void,
-    handleClick:(index:number) => void
-  };
+  backgroundColor: string;
+  layout: any;
+  gridItems: any;
+  onLayoutChange?(layout: GridLayout.Layout[]): void;
+  handleClick: (index: number) => void;
+};
 
-  export const drawerWidth = 240;
+export const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-    drawerPaper: {
-      width: drawerWidth,
-      marginTop:64,
-    },
-    subContent: {
-      border: "1px solid black",
-      backgroundColor: ({ backgroundColor }: any) => backgroundColor,
-      color: "white",
-    },
-  }));
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    marginTop: 64,
+  },
+  subContent: {
+    border: "1px solid black",
+    backgroundColor: ({ backgroundColor }: any) => backgroundColor,
+    color: "white",
+  },
+}));
 
-
-const SlideNavigation = ({backgroundColor,layout,gridItems,onLayoutChange,handleClick}:SlideNavigatorProp) => {
-    const classes = useStyles({backgroundColor});
-    return (
-        <div >
-           <Drawer
+const SlideNavigation = ({
+  backgroundColor,
+  layout,
+  gridItems,
+  onLayoutChange,
+  handleClick,
+}: SlideNavigatorProp) => {
+  const classes = useStyles({ backgroundColor });
+  const renderItem = (type: ActionProp, content: string) => {
+    switch (type) {
+      case ActionProp.photo:
+        return (
+          <div style={{ width: "10vw", height: "10vh" }}>
+            <PhotoImage imageSource={content} />
+          </div>
+        );
+      case ActionProp.video:
+        return (
+          <div style={{ width: "10vw", height: "10vh" }}>
+            <Video videoSource={content} />
+          </div>
+        );
+      case ActionProp.text:
+        return (
+          <TextContainer>
+            <div
+              style={{
+                color: "#000",
+              }}
+            >
+              {content}
+            </div>
+          </TextContainer>
+        );
+      default:
+        return null;
+    }
+  };
+  return (
+    <div>
+      <Drawer
         className={classes.drawer}
         variant="permanent"
         classes={{
@@ -59,39 +93,22 @@ const SlideNavigation = ({backgroundColor,layout,gridItems,onLayoutChange,handle
             isResizable={true}
             onLayoutChange={onLayoutChange}
           >
-            {layout.map((item:any, i:number) => (
-              <div key={item.i} className={classes.subContent} onClick={() => handleClick(i)}>
+            {layout.map((item: any, i: number) => (
+              <div
+                key={item.i}
+                className={classes.subContent}
+                onClick={() => handleClick(i)}
+              >
                 {gridItems[i].map((item: any, i: number) => (
-                  <div key={item.id}>
-                    {item.type === ActionProp.photo ? (
-                      <div style={{ width: "10vw", height: "10vh" }}>
-                        <PhotoImage imageSource={item.content} />
-                      </div>
-                    ) : item.type === ActionProp.video ? (
-                      <div style={{ width: "10vw", height: "10vh" }}>
-                        <Video videoSource={item.content} />
-                      </div>
-                    ) : item.type === ActionProp.text ? (
-                      <TextContainer>
-                        <div
-                          style={{
-                            color: "#000",
-                          }}
-                          
-                        >
-                          {item.content}
-                        </div>
-                      </TextContainer>
-                    ) : null}
-                  </div>
+                  <div key={item.id}>{renderItem(item.type, item.content)}</div>
                 ))}
               </div>
             ))}
           </GridLayout>
         </div>
       </Drawer>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default SlideNavigation
+export default SlideNavigation;
